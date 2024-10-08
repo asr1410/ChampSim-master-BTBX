4e10d01291658b5f215fab9a438acd88a642a7fa
asr1410 <abhishek.rawat1410@gmail.com>
2024-10-08 17:14:58 +0530

    PreRequisite Done

---
+++ client_007_fdip_BTBX.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+cd /ChampSim-master-BTBX
+./run_champsim.sh hashed_perceptron-BTBX-fdip-next_line-spp_dev-no-lru-1core 50 50 client_007.champsimtrace.xz
--- launch/launch.sh
+++ launch/launch.sh
@@ -20,33 +20,33 @@ bench[12]=server_009
 bench[13]=server_010
 bench[14]=server_011
 bench[15]=server_012
-bench[16]=server_013
-bench[17]=server_014
-bench[18]=server_015
-bench[19]=server_016
-bench[20]=server_017
-bench[21]=server_018
-bench[22]=server_019
-bench[23]=server_020
-bench[24]=server_021
-bench[25]=server_022
-bench[26]=server_023
-bench[27]=server_024
-bench[28]=server_025
-bench[29]=server_026
-bench[30]=server_027
-bench[31]=server_028
-bench[32]=server_029
-bench[33]=server_030
-bench[34]=server_031
-bench[35]=server_032
-bench[36]=server_033
-bench[37]=server_034
-bench[38]=server_035
-bench[39]=server_036
-bench[40]=server_037
-bench[41]=server_038
-bench[42]=server_039
+# bench[16]=server_013
+# bench[17]=server_014
+# bench[18]=server_015
+# bench[19]=server_016
+# bench[20]=server_017
+# bench[21]=server_018
+# bench[22]=server_019
+# bench[23]=server_020
+# bench[24]=server_021
+# bench[25]=server_022
+# bench[26]=server_023
+# bench[27]=server_024
+# bench[28]=server_025
+# bench[29]=server_026
+# bench[30]=server_027
+# bench[31]=server_028
+# bench[32]=server_029
+# bench[33]=server_030
+# bench[34]=server_031
+# bench[35]=server_032
+# bench[36]=server_033
+# bench[37]=server_034
+# bench[38]=server_035
+# bench[39]=server_036
+# bench[40]=server_037
+# bench[41]=server_038
+# bench[42]=server_039
 
 prefetch[0]=no
 prefetch[1]=fdip
@@ -56,12 +56,22 @@ btb[0]=convBTB
 btb[1]=pdede
 btb[2]=BTBX
 
-for ((j=0;j<2;j=j+1)); do
-    for ((i=0;i<43;i=i+1)); do
-	for ((k=0;k<3;k=k+1)); do
-            echo "sbatch --partition=CPUQ --account=share-ie-idi --nodes=1 --ntasks-per-node=1 --mem=3GB --time=0-0:30:00 --job-name=${bench[i]}_${prefetch[j]}_${btb[k]} scripts/${bench[i]}_${prefetch[j]}_${btb[k]}.sh"
-            #sbatch --partition=CPUQ --account=share-ie-idi --nodes=1 --ntasks-per-node=1 --mem=3GB --time=0-0:30:00 --job-name=${bench[i]}_${prefetch[j]}_${btb[k]} scripts/${bench[i]}_${prefetch[j]}_${btb[k]}.sh
-	    <cluster_launch_command_here>
-	done
+for ((j=0; j<2; j++)); do
+    for ((i=0; i<16; i++)); do
+        for ((k=0; k<3; k++)); do
+            echo "Running: ./${bench[i]}_${prefetch[j]}_${btb[k]}.sh"
+            # Execute the script directly
+            ./${bench[i]}_${prefetch[j]}_${btb[k]}.sh
+        done
     done
-done 
+done
+
+# for ((j=0;j<2;j=j+1)); do
+#     for ((i=0;i<43;i=i+1)); do
+# 	for ((k=0;k<3;k=k+1)); do
+#             echo "sbatch --partition=CPUQ --account=share-ie-idi --nodes=1 --ntasks-per-node=1 --mem=3GB --time=0-0:30:00 --job-name=${bench[i]}_${prefetch[j]}_${btb[k]} scripts/${bench[i]}_${prefetch[j]}_${btb[k]}.sh"
+#             #sbatch --partition=CPUQ --account=share-ie-idi --nodes=1 --ntasks-per-node=1 --mem=3GB --time=0-0:30:00 --job-name=${bench[i]}_${prefetch[j]}_${btb[k]} scripts/${bench[i]}_${prefetch[j]}_${btb[k]}.sh
+# 	    <cluster_launch_command_here>
+# 	done
+#     done
+# done 
