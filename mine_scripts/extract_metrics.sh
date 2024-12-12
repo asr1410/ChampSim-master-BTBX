37442f0ecb4a14252c7d776185c0ce6f23b30062
asr1410 <abhishek.rawat1410@gmail.com>
2024-12-12 23:12:26 +0530

    updated custom script in mine_script folder

--- mine_scripts/extract_metrics.sh
+++ mine_scripts/extract_metrics.sh
@@ -1,35 +1,7 @@
-# #!/bin/bash
-
-# folders=(
-#     "results_5M"
-#     "results_10M" 
-#     "results_15M"
-#     "results_20M"
-#     "results_25M"
-#     "results_30M"
-#     "results_35M"
-#     "results_40M"
-#     "results_45M"
-#     "results_50M"
-# )
-
-# for folder in "${folders[@]}"; do
-#     if [ -d "$folder" ]; then
-#         # Process each file in folder
-#         for file in "$folder"/*; do
-#             if [ -f "$file" ]; then
-#                 grep "CPU 0 Branch Prediction Accuracy:" "$file" 2>/dev/null | \
-#                 awk '{print $6}' | sed 's/%//'
-#             fi
-#         done
-#     fi
-# done
-
-
 #!/bin/bash
 
 # Directory containing the trace files
-directory="./results_50M"
+directory="./results/static_allocation"
 
 # Arrays to store values
 declare -a names=()
