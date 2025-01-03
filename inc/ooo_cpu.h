e9b4d8405a7e277273b54f0545f12f3d94038ba0
asr1410 <abhishek.rawat1410@gmail.com>
2025-01-03 17:36:34 +0000

    formatted and remove comments from code

--- inc/ooo_cpu.h
+++ inc/ooo_cpu.h
@@ -12,7 +12,6 @@
 
 using namespace std;
 
-// CORE PROCESSOR
 #define FETCH_WIDTH 6
 #define DECODE_WIDTH 6
 #define EXEC_WIDTH 6
@@ -21,32 +20,26 @@ using namespace std;
 #define RETIRE_WIDTH 4
 #define SCHEDULER_SIZE 128
 #define BRANCH_MISPREDICT_PENALTY 1
-//#define SCHEDULING_LATENCY 0
-//#define EXEC_LATENCY 0
-//#define DECODE_LATENCY 2
 
 #define STA_SIZE (ROB_SIZE*NUM_INSTR_DESTINATIONS_SPARC)
 
 extern uint32_t SCHEDULING_LATENCY, EXEC_LATENCY, DECODE_LATENCY;
 
 struct BTB_outcome {
-		uint64_t target;
-		uint8_t branch_type;
-		uint8_t sequential_BTB_access;
+  uint64_t target;
+  uint8_t branch_type;
+  uint8_t sequential_BTB_access;
 };
 
-// cpu
 class O3_CPU {
   public:
-	bool skip_next_cycle;
+    bool skip_next_cycle;
     uint32_t cpu;
 
-    // trace
     FILE *trace_file;
     char trace_string[1024];
     char gunzip_command[1024];
 
-    // instruction
     input_instr next_instr;
     input_instr current_instr;
     cloudsuite_instr current_cloudsuite_instr;
@@ -59,30 +52,24 @@ class O3_CPU {
     uint32_t inflight_reg_executions, inflight_mem_executions, num_searched;
     uint32_t next_ITLB_fetch;
 
-    // reorder buffer, load/store queue, register file
-    CORE_BUFFER IFETCH_BUFFER{"IFETCH_BUFFER", /*128*/FETCH_WIDTH*2};
+    CORE_BUFFER IFETCH_BUFFER{"IFETCH_BUFFER", 128};
     CORE_BUFFER DECODE_BUFFER{"DECODE_BUFFER", DECODE_WIDTH*3};
     CORE_BUFFER ROB{"ROB", ROB_SIZE};
     LOAD_STORE_QUEUE LQ{"LQ", LQ_SIZE}, SQ{"SQ", SQ_SIZE};
 
-    // store array, this structure is required to properly handle store instructions
     uint64_t STA[STA_SIZE], STA_head, STA_tail; 
 
-    // Ready-To-Execute
     uint32_t RTE0[ROB_SIZE], RTE0_head, RTE0_tail, 
              RTE1[ROB_SIZE], RTE1_head, RTE1_tail;  
 
-    // Ready-To-Load
     uint32_t RTL0[LQ_SIZE], RTL0_head, RTL0_tail, 
              RTL1[LQ_SIZE], RTL1_head, RTL1_tail;  
 
-    // Ready-To-Store
     uint32_t RTS0[SQ_SIZE], RTS0_head, RTS0_tail,
              RTS1[SQ_SIZE], RTS1_head, RTS1_tail;
 
-    // branch
-    int branch_mispredict_stall_fetch; // flag that says that we should stall because a branch prediction was wrong
-    int mispredicted_branch_iw_index; // index in the instruction window of the mispredicted branch.  fetch resumes after the instruction at this index executes
+    int branch_mispredict_stall_fetch;
+    int mispredicted_branch_iw_index;
     uint8_t  fetch_stall;
     uint64_t fetch_resume_cycle;
     uint64_t nextInstPC;
@@ -90,17 +77,15 @@ class O3_CPU {
     uint64_t num_branch, branch_mispredictions, branch_direction_mispredictions, branch_target_mispredictions1, branch_target_mispredictions2;
     uint64_t mispredict_on_btb_hit;
     uint64_t total_rob_occupancy_at_branch_mispredict;
-  uint64_t total_branch_types[8];
-  uint64_t mispredicted_branch_types[8];
-  uint64_t BTB_reads, BTB_writes, PageBTB_reads, PageBTB_writes, PageBTB_readsBeforeWrite, RegionBTB_reads, RegionBTB_writes, RegionBTB_readsBeforeWrite;
-  std::map<uint64_t, std::set<uint64_t>> branchesInCB;
-  std::set<uint64_t> unique_branchPC;
+    uint64_t total_branch_types[8];
+    uint64_t mispredicted_branch_types[8];
+    uint64_t BTB_reads, BTB_writes, PageBTB_reads, PageBTB_writes, PageBTB_readsBeforeWrite, RegionBTB_reads, RegionBTB_writes, RegionBTB_readsBeforeWrite;
+    std::map<uint64_t, std::set<uint64_t>> branchesInCB;
+    std::set<uint64_t> unique_branchPC;
     uint64_t offset_size[132];
     uint64_t BTB_4D_Hits, BTB_6D_Hits, BTB_8D_Hits, BTB_12D_Hits, BTB_18D_Hits, BTB_25D_Hits, BTB_46D_Hits, BTB_Ret_Hits;
     uint64_t BTB_4D_Misses, BTB_6D_Misses, BTB_8D_Misses, BTB_12D_Misses, BTB_18D_Misses, BTB_25D_Misses, BTB_46D_Misses, BTB_Ret_Misses;
-           
 
-    // TLBs and caches
     CACHE ITLB{"ITLB", ITLB_SET, ITLB_WAY, ITLB_SET*ITLB_WAY, ITLB_WQ_SIZE, ITLB_RQ_SIZE, ITLB_PQ_SIZE, ITLB_MSHR_SIZE},
           DTLB{"DTLB", DTLB_SET, DTLB_WAY, DTLB_SET*DTLB_WAY, DTLB_WQ_SIZE, DTLB_RQ_SIZE, DTLB_PQ_SIZE, DTLB_MSHR_SIZE},
           STLB{"STLB", STLB_SET, STLB_WAY, STLB_SET*STLB_WAY, STLB_WQ_SIZE, STLB_RQ_SIZE, STLB_PQ_SIZE, STLB_MSHR_SIZE},
@@ -108,102 +93,93 @@ class O3_CPU {
           L1D{"L1D", L1D_SET, L1D_WAY, L1D_SET*L1D_WAY, L1D_WQ_SIZE, L1D_RQ_SIZE, L1D_PQ_SIZE, L1D_MSHR_SIZE},
           L2C{"L2C", L2C_SET, L2C_WAY, L2C_SET*L2C_WAY, L2C_WQ_SIZE, L2C_RQ_SIZE, L2C_PQ_SIZE, L2C_MSHR_SIZE};
 
-  // trace cache for previously decoded instructions
-  
-    // constructor
     O3_CPU() {
-        cpu = 0;
+      cpu = 0;
 
-        // trace
-        trace_file = NULL;
-        skip_next_cycle = false;
+      trace_file = NULL;
+      skip_next_cycle = false;
 
-        // instruction
-        instr_unique_id = 0;
-        completed_executions = 0;
-        begin_sim_cycle = 0;
-        begin_sim_instr = 0;
-        last_sim_cycle = 0;
-        last_sim_instr = 0;
-        finish_sim_cycle = 0;
-        finish_sim_instr = 0;
-        warmup_instructions = 0;
-        simulation_instructions = 0;
-        instrs_to_read_this_cycle = 0;
-        instrs_to_fetch_this_cycle = 0;
+      instr_unique_id = 0;
+      completed_executions = 0;
+      begin_sim_cycle = 0;
+      begin_sim_instr = 0;
+      last_sim_cycle = 0;
+      last_sim_instr = 0;
+      finish_sim_cycle = 0;
+      finish_sim_instr = 0;
+      warmup_instructions = 0;
+      simulation_instructions = 0;
+      instrs_to_read_this_cycle = 0;
+      instrs_to_fetch_this_cycle = 0;
 
-        next_print_instruction = STAT_PRINTING_PERIOD;
-        num_retired = 0;
+      next_print_instruction = STAT_PRINTING_PERIOD;
+      num_retired = 0;
 
-        inflight_reg_executions = 0;
-        inflight_mem_executions = 0;
-        num_searched = 0;
+      inflight_reg_executions = 0;
+      inflight_mem_executions = 0;
+      num_searched = 0;
 
-        next_ITLB_fetch = 0;
+      next_ITLB_fetch = 0;
 
-        // branch
-        branch_mispredict_stall_fetch = 0;
-        mispredicted_branch_iw_index = 0;
-        fetch_stall = 0;
-	fetch_resume_cycle = 0;
-        nextInstPC = 0;
-        IBUF_occupancy = 0;
-        num_branch = 0;
-        branch_mispredictions = 0;
-        branch_direction_mispredictions = 0;
-        branch_target_mispredictions1 = 0;
-        branch_target_mispredictions2 = 0;
-        mispredict_on_btb_hit = 0;
-	for(uint32_t i=0; i<8; i++)
-	  {
-	    total_branch_types[i] = 0;
-	    mispredicted_branch_types[i] = 0;
-	  }
-	BTB_reads = BTB_writes = PageBTB_reads = PageBTB_writes = PageBTB_readsBeforeWrite = RegionBTB_reads = RegionBTB_writes = RegionBTB_readsBeforeWrite = 0;
-	branchesInCB.clear();
-	unique_branchPC.clear();
-	for(uint32_t i=0; i<132; i++)
-	  {
-	    offset_size[i] = 0;
-	  }
+      branch_mispredict_stall_fetch = 0;
+      mispredicted_branch_iw_index = 0;
+      fetch_stall = 0;
+      fetch_resume_cycle = 0;
+      nextInstPC = 0;
+      IBUF_occupancy = 0;
+      num_branch = 0;
+      branch_mispredictions = 0;
+      branch_direction_mispredictions = 0;
+      branch_target_mispredictions1 = 0;
+      branch_target_mispredictions2 = 0;
+      mispredict_on_btb_hit = 0;
+      for (uint32_t i = 0; i < 8; i++) {
+        total_branch_types[i] = 0;
+        mispredicted_branch_types[i] = 0;
+      }
+      BTB_reads = BTB_writes = PageBTB_reads = PageBTB_writes = PageBTB_readsBeforeWrite = RegionBTB_reads = RegionBTB_writes = RegionBTB_readsBeforeWrite = 0;
+      branchesInCB.clear();
+      unique_branchPC.clear();
+      for (uint32_t i = 0; i < 132; i++) {
+        offset_size[i] = 0;
+      }
 
-	BTB_4D_Hits = BTB_6D_Hits = BTB_8D_Hits = BTB_12D_Hits = BTB_18D_Hits = BTB_25D_Hits = BTB_46D_Hits = BTB_Ret_Hits = 0;
-	BTB_4D_Misses = BTB_6D_Misses = BTB_8D_Misses = BTB_12D_Misses = BTB_18D_Misses = BTB_25D_Misses = BTB_46D_Misses = BTB_Ret_Misses = 0;
-	
-        for (uint32_t i=0; i<STA_SIZE; i++)
-	  STA[i] = UINT64_MAX;
-        STA_head = 0;
-        STA_tail = 0;
+      BTB_4D_Hits = BTB_6D_Hits = BTB_8D_Hits = BTB_12D_Hits = BTB_18D_Hits = BTB_25D_Hits = BTB_46D_Hits = BTB_Ret_Hits = 0;
+      BTB_4D_Misses = BTB_6D_Misses = BTB_8D_Misses = BTB_12D_Misses = BTB_18D_Misses = BTB_25D_Misses = BTB_46D_Misses = BTB_Ret_Misses = 0;
 
-        for (uint32_t i=0; i<ROB_SIZE; i++) {
-	  RTE0[i] = ROB_SIZE;
-	  RTE1[i] = ROB_SIZE;
-        }
-        RTE0_head = 0;
-        RTE1_head = 0;
-        RTE0_tail = 0;
-        RTE1_tail = 0;
+      for (uint32_t i = 0; i < STA_SIZE; i++)
+        STA[i] = UINT64_MAX;
+      STA_head = 0;
+      STA_tail = 0;
 
-        for (uint32_t i=0; i<LQ_SIZE; i++) {
-	  RTL0[i] = LQ_SIZE;
-	  RTL1[i] = LQ_SIZE;
-        }
-        RTL0_head = 0;
-        RTL1_head = 0;
-        RTL0_tail = 0;
-        RTL1_tail = 0;
+      for (uint32_t i = 0; i < ROB_SIZE; i++) {
+        RTE0[i] = ROB_SIZE;
+        RTE1[i] = ROB_SIZE;
+      }
+      RTE0_head = 0;
+      RTE1_head = 0;
+      RTE0_tail = 0;
+      RTE1_tail = 0;
 
-        for (uint32_t i=0; i<SQ_SIZE; i++) {
-	  RTS0[i] = SQ_SIZE;
-	  RTS1[i] = SQ_SIZE;
-        }
-        RTS0_head = 0;
-        RTS1_head = 0;
-        RTS0_tail = 0;
-        RTS1_tail = 0;
+      for (uint32_t i = 0; i < LQ_SIZE; i++) {
+        RTL0[i] = LQ_SIZE;
+        RTL1[i] = LQ_SIZE;
+      }
+      RTL0_head = 0;
+      RTL1_head = 0;
+      RTL0_tail = 0;
+      RTL1_tail = 0;
+
+      for (uint32_t i = 0; i < SQ_SIZE; i++) {
+        RTS0[i] = SQ_SIZE;
+        RTS1[i] = SQ_SIZE;
+      }
+      RTS0_head = 0;
+      RTS1_head = 0;
+      RTS0_tail = 0;
+      RTS1_tail = 0;
     }
 
-    // functions
     void read_from_trace(),
          spec_inst_prefetch(),
          fetch_instruction(),
@@ -246,25 +222,23 @@ class O3_CPU {
 
     uint32_t check_and_add_lsq(uint32_t rob_index);
 
-    // branch predictor
     uint8_t predict_branch(uint64_t ip);
     void    initialize_branch_predictor(),
             last_branch_result(uint64_t ip, uint8_t taken);
 
     BTB_outcome btb_prediction(uint64_t ip, uint8_t branch_type);
     void initialize_btb(),
-      update_btb(uint64_t ip, uint64_t branch_target, uint8_t taken, uint8_t branch_type);
+         update_btb(uint64_t ip, uint64_t branch_target, uint8_t taken, uint8_t branch_type);
 
-  // code prefetching
-  void l1i_prefetcher_initialize();
-  void l1i_prefetcher_branch_operate(uint64_t ip, uint8_t branch_type, uint64_t branch_target);
-  void l1i_prefetcher_instruction_operate(uint64_t ip);
-  void l1i_prefetcher_instruction_spec_operate(uint64_t ip);
-  void l1i_prefetcher_cache_operate(uint64_t v_addr, uint8_t cache_hit, uint8_t prefetch_hit);
-  void l1i_prefetcher_cycle_operate();
-  void l1i_prefetcher_cache_fill(uint64_t v_addr, uint32_t set, uint32_t way, uint8_t prefetch, uint64_t evicted_v_addr);
-  void l1i_prefetcher_final_stats();
-  int prefetch_code_line(uint64_t pf_v_addr); 
+    void l1i_prefetcher_initialize();
+    void l1i_prefetcher_branch_operate(uint64_t ip, uint8_t branch_type, uint64_t branch_target);
+    void l1i_prefetcher_instruction_operate(uint64_t ip);
+    void l1i_prefetcher_instruction_spec_operate(uint64_t ip);
+    void l1i_prefetcher_cache_operate(uint64_t v_addr, uint8_t cache_hit, uint8_t prefetch_hit);
+    void l1i_prefetcher_cycle_operate();
+    void l1i_prefetcher_cache_fill(uint64_t v_addr, uint32_t set, uint32_t way, uint8_t prefetch, uint64_t evicted_v_addr);
+    void l1i_prefetcher_final_stats();
+    int prefetch_code_line(uint64_t pf_v_addr); 
 };
 
 extern O3_CPU ooo_cpu[NUM_CPUS];
