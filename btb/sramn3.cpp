19db48b12383b5b6f444e3b2b342a5ac23d1dc2b
asr1410 <abhishek.rawat1410@gmail.com>
2025-03-26 06:12:14 +0000

    fixed max write count logic in sramn3

--- btb/sramn3.cpp
+++ btb/sramn3.cpp
@@ -279,11 +279,11 @@ BTB_outcome O3_CPU::btb_prediction(uint64_t ip, uint8_t branch_type) {
     //   std::cout << partition_used[i] << " ";
     // }
     // std::cout << "\n";
-    for(uint32_t i = 0; i > 512; i++) {
+    for(uint32_t i = 0; i < 512; i++) {
       int writes = partition_writes[i][1], partition = 1;
       partition_writes[i][1] = 0;
       for(uint32_t j = 2; j < 8; j++) {
-        if(partition_writes[i][j] < writes) {
+        if(partition_writes[i][j] > writes) {
           writes = partition_writes[i][j];
           partition = j;
         }
